6ea2937c16b8bd4fa5f6fcf69c80238ddd67e2fb
Alex Gilbert <agilbe@uw.edu>
2016-03-07 12:54:36 -0800

    s

--- memaid.js
+++ memaid.js
@@ -1,13 +1,13 @@
 (function() {
 	"use strict";
 
-
+	var keyArray = [];
+	var defArray = [];
 	
 
 	window.onload = function() {
-		
 		loadTeams();
-		//document.onclick = loadTeams;
+		document.onclick = showTeams;
 	};
 
 	function $(element) {
@@ -22,13 +22,32 @@
 	}
 
 	function loadTeams() {
-		ajaxHelper(displayTeams, "/memaid.json");
+		ajaxHelper(populateTeams, "http://agilbe.github.io/memaid.json");
 	}
 
-	function displayTeams() {
-		alert("hello");
-		teamJSON = JSON.parse(this.responseText);
-		alert(teamJSON[0]);
+	function populateTeams() {
+		var team = JSON.parse(this.responseText);
+		keyArray = Object.keys(team);
+		for (var i = 0; i < keyArray.length; i++) {
+			defArray.push(team[keyArray[i]]);
+		}
+		showTeams();
+	}
+
+	function showTeams() {
+		$("main").innerHTML = "";
+		var rando = Math.round(Math.floor(Math.random() * keyArray.length)); 
+		//alert(rando);
+		var titleDiv = document.createElement("div");
+		titleDiv.id = "keyword";
+		titleDiv.style.textAlign = "center";
+		titleDiv.innerHTML = keyArray[rando];
+		var defDiv = document.createElement("div");
+		defDiv.id = "def";
+		defDiv.style.textAlign = "center";
+		defDiv.innerHTML = defArray[rando];
+		$("main").appendChild(titleDiv);
+		$("main").appendChild(defDiv);
 	}
 
 })();
