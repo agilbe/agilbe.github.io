9ff79ee98691b25aa867432ebc7f31e8a8894b04
Alex Gilbert <agilbe@uw.edu>
2016-03-06 21:17:36 -0800

    s

--- pokedex.js
+++ pokedex.js
@@ -4,10 +4,21 @@
 	var BASE_URL = "http://pokeapi.co/api/v2/";
 	var BASE_URL_OLD = "http://pokeapi.co/api/v1/";
 	var CHANGED_URL = "http://pokeapi.co/api/v2/pokemon/?limit=100000";
-
+	var pokeArray = [];
+	var urlArray = [];
+	var nameArray = [];
+	var currentSpeciesInfo = "";
+		
+	
 	window.onload = function() {
 		loadPokes();
+		$("search").oninput = searchPokes;
+		document.onclick = focusBox;
 	};
+
+	function focusBox() {
+		$("search").focus();
+	}
 
 	function $(element) {
 		return document.getElementById(element);
@@ -34,9 +45,6 @@
 
 	function displayPokes() {
 		var pokeInfo = JSON.parse(this.responseText).results;
-		var pokeArray = [];
-		var urlArray = [];
-		var nameArray = [];
 		for (var i = 0; i < pokeInfo.length; i++) {
 			pokeArray.push(pokeInfo[i].name);
 			urlArray.push(pokeInfo[i].url);
@@ -48,6 +56,7 @@
 			}
 			var pokeDiv = document.createElement("div");
 			var pokeTitle = document.createElement("span");
+			pokeTitle.className = "pokeSpan";
 			var pokeName = pokeArray[i]
 			if (pokeArray[i]=="nidoran-f"||pokeArray[i]=="nidoran-m"||pokeArray[i]=="mr-mime"||pokeArray[i]=="mime-jr"||pokeArray[i]=="porygon-z"){
 				pokeTitle.innerHTML = capitalizeFirstLetter(printUntil(pokeArray[i], "-")) + "-" + capitalizeFirstLetter(pokeArray[i].split("-")[1]);
@@ -89,9 +98,68 @@
 	}
 
 	function showData() {
-		alert("fuck you, this Pokemon is #" + this.id + " in the national Pokedex");
+		//alert("fuck you, this Pokemon is #" + this.id + " in the national Pokedex");
+		$("search").value = '';
+		$("search").focus();
+		ajaxHelper(displayInfo, BASE_URL + "pokemon/" + this.id + "/");
 	}
 
+	function displayInfo() {
+		var specPokeInfo = JSON.parse(this.responseText);
+		$("tester").innerHTML = "";
+		//$("tester").innerHTML = this.responseText;
+		$("main").innerHTML = "";
+		//alert(specPokeInfo.id - 1);
+		loadSpeciesInfo(specPokeInfo.id);
+		//$("tester").innerHTML = currentSpeciesInfo;
+		//var currentSpeciesJson = JSON.parse(currentSpeciesInfo);
+		//alert(currentSpeciesJson.id);
+		var overviewDiv = document.createElement("div");
+		overviewDiv.id = "overview";
+		overviewDiv.style.textAlign = "center";
+		//overviewDiv.style.fontWeight = "bold";
+		var overviewSpan = document.createElement("h2");
+		var overviewImg = document.createElement("img");
+		overviewImg.src = "http://www.pokestadium.com/assets/img/sprites/official-art/large/" + nameArray[specPokeInfo.id - 1] + ".png";
+		//overviewImg.src = "http://www.pokestadium.com/sprites/xy/" + nameArray[specPokeInfo.id - 1] + ".gif"
+		overviewImg.alt = nameArray[specPokeInfo.id - 1];
+		overviewImg.style.width = "25%";
+		overviewImg.style.height = "25%";
+		var overviewDesc = document.createElement("div");
+		//overviewDesc.innerHTML = currentSpeciesInfo.flavor_text_entries;
+		overviewSpan.innerHTML = capitalizeFirstLetter(nameArray[specPokeInfo.id - 1]);
+		overviewDiv.appendChild(overviewSpan);
+		overviewDiv.appendChild(overviewImg);
+		$("main").appendChild(overviewDiv);
+	}
+
+	function loadSpeciesInfo(id) {
+		ajaxHelper(pullSpecies, BASE_URL + "pokemon-species/" + id + "/"); 
+	}
+
+	function pullSpecies() {
+		currentSpeciesInfo = this.responseText;
+		//$("tester").innerHTML = currentSpeciesInfo;
+	}
+
+	function searchPokes() {
+		//alert ("hi");
+		var searchName = this.value.toLowerCase();
+		var allNames = document.querySelectorAll(".pokeSpan");
+		//var allNames = document.getElementsByClassName("pokeSpan");
+		//alert(this.value);
+		//alert(allNames[0].innerHTML);
+		for (var i = 0; i < allNames.length; i++) {
+			if (allNames[i].innerHTML.toLowerCase().indexOf(searchName) > -1) {
+				//alert(allNames[i].innerHTML);
+				//alert(i+1);
+				$(i+1).style.display = "block";
+
+			} else {
+				$(i+1).style.display = "none";
+			}
+		}
+	}
 
 
 })();
