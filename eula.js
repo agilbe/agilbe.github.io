6c33662cd0f0e4421e6333fd009a981f4890386c
Alex Gilbert <agilbe@uw.edu>
2016-05-30 19:13:31 -0700

    yes

--- eula.js
+++ eula.js
@@ -21,13 +21,19 @@
 	function showBigBrother() {
 		var person = prompt("Please enter your name:");
 		if (person != "" && person!= null) {
-			$("main").innerHTML = "Welcome, " + person + ", to your new home."
+			$("main").innerHTML = "";
+			var heading = document.createElement("h2");
+			heading.innerHTML = "Welcome, " + person + ", to your new home.";
 			var div = document.createElement("div");
 			var image = document.createElement("img");
+			var break1 = document.createElement("br");
 			image.src = "https://elliotchan.files.wordpress.com/2016/03/big_brother_is_watching_wide.png";
+			div.appendChild(break1);
+			div.appendChild(heading);
+			div.appendChild(break1);
 			div.appendChild(image);
 			$("main").appendChild(div);
-			
+			document.onclick = null;
 
 		}
 	}
