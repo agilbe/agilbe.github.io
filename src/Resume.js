1b6a65d9c53474c2c4fe4c3643fa57ca2669e7c7
Alex Gilbert <agilbe@uw.edu>
2016-11-29 01:27:39 -0800

    updated resume page

--- src/Resume.js
+++ src/Resume.js
@@ -4,6 +4,7 @@ var Resume = React.createClass({
 	render() {
 		return(<div className="resume container">
 			<div className="pageTitle">resume</div>
+			<div>click on the resume to download it!</div>
 			<div className="resumeEmbed">
 				<a href="data/resume2.pdf" download="data/resume2.pdf" target="_blank"><img id="resumePDF" src="data/resume2pic.jpg" alt="resume" /></a>
 			</div>
